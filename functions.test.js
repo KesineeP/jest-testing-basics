6439ec48a5f082c8b3eff5ffc20e3402ab6ab829
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-11-23 19:54:23 -0500

    Basic testing

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,43 @@ test("Add 2 + 2 to equal 4", () => {
 test("Add 2 + 2 to NOT equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
+
+//CHECK FOR TRUTHY & FALSY VALUES
+//toBeNull matches only null
+//toBeUndefined matches only undefined
+//toBeDefined is the opposite of toBeUndefined
+//toBeTruthy matches anything that an if statement treats as true
+//toBeFalsy matches anything that an if statement treats as false
+
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test("Should be falsy", () => {
+  expect(functions.checkValue()).toBeFalsy();
+});
+
+test("User should be Will Price object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Will",
+    lastName: "Price",
+  });
+});
+
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+  //toBeLessThanOrEqual()
+});
+
+//REGEX
+test("There is no I in team", () => {
+  expect("teami").not.toMatch(/I/i);
+});
+
+//ARRAY
+test("Admin should be in username", () => {
+  usernames = ["john", "karen", "admin"];
+  expect(usernames).toContain("admin");
+});
